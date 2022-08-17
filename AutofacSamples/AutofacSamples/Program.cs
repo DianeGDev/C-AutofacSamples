0dfa94662b89f2c30391d875b9a39ec541e5e177
DianeGDev@gmail.com <DianeGDev@gmail.com>
2022-08-17 14:41:04 -0500

    Autofac samples 08-17-2022

---
+++ AutofacSamples/AutofacSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutofacSamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
